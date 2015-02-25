2d3f097fd777e926a82bc6fcd3d6715f0f6c0bf3
Tony Morris <tmorris@tmorris.net>
2015-02-25 12:28:40 +1000

    json

--- src/Data/Json/Json.hs
+++ src/Data/Json/Json.hs
@@ -1,2 +1,29 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Data.Json.Json where
-  
+
+import Data.Bool(Bool)
+import Data.Data(Data)
+import Data.Eq(Eq)
+import Data.Map(Map)
+import Data.Ord(Ord)
+import Data.Typeable(Typeable)
+
+import Prelude(Show)
+
+-- todo parameterise?
+type JsonMap =
+  Map
+
+-- todo parameterise?
+type JsonArray =
+  []
+
+data Json s n =
+  Object (JsonMap s (Json s n))
+  | Array (JsonArray (Json s n))
+  | String s
+  | Number n
+  | Boolean Bool
+  | Null
+  deriving (Eq, Ord, Show, Data, Typeable)
